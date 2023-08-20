bd8d0f3c61a5908854a8a4a2e7164c6e835b8999
MohammedGhaly <medooghaly7@gmail.com>
2023-08-20 18:14:06 +0300

    DBStorage created

--- console.py
+++ console.py
@@ -74,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -125,14 +125,15 @@ class HBNBCommand(cmd.Cmd):
         if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
+
         new_instance = HBNBCommand.classes[class_name]()
-        
+
         if not len(arguments) == 0:
             pattern = r'(\w+)\s*=\s*"([^"]*)"|(\w+)\s*=\s*([-\d.]+)'
             matches = re.findall(pattern, args)
-            parameters_list = [(match[0] or match[2], match[1] or match[3]) for match in matches]
-        
+            parameters_list = [(match[0] or match[2], match[1] or match[3])
+                               for match in matches]
+
             for element in parameters_list:
                 key = element[0]
                 typ = type((HBNBCommand.classes[class_name].__dict__[key]))
@@ -338,6 +339,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
