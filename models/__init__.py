452b3d5dc61de13c154d5924f2410ba6d8f6b994
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-08-21 15:34:34 +0100

    Task 8

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from models.city import City
 from models.base_model import BaseModel
 from models.user import User
-# from models.place import Place
+from models.place import Place
 # from models.review import Review
 from models.state import State
 # from models.amenity import Amenity
--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from os import getenv
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -14,6 +15,7 @@ class City(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities")
     else:
         state_id = ''
         name = ''
