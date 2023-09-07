8739a49e21849938dd94cb4319dc7e529b2bc397
MohammedGhaly <medooghaly7@gmail.com>
2023-09-07 06:18:33 -0700

    task 2 solved

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+a script that distributes an archive to web servers using (do_deploy)
+"""
+
+from fabric.api import run, put, env
+import os
+
+env.hosts = ['100.25.36.194', '54.89.117.225']
+
+
+def do_deploy(archive_path):
+    """
+    uploads an archive to your web servers
+    """
+    if os.path.exists(archive_path) is False:
+        return(False)
+    try:
+        put(archive_path, '/tmp/')
+        file = archive_path.split('/')[-1]
+        archive_name = file.split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+            (file, archive_name))
+        run('rm /tmp/{}'.format(file))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(archive_name, archive_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+            .format(archive_name))
+        return(True)
+    except:
+        return(False)
