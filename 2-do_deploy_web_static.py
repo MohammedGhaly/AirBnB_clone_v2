330fce2ada58efb7cd47564efe0cf6a2d2d22d1b
MohammedGhaly <medooghaly7@gmail.com>
2023-09-07 07:26:13 -0700

    fixed return on failure

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,20 +16,31 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return(False)
     try:
-        put(archive_path, '/tmp/')
+        if put(archive_path, '/tmp/').failed is True:
+            return False
         file = archive_path.split('/')[-1]
         archive_name = file.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
-            (file, archive_name))
-        run('rm /tmp/{}'.format(file))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(archive_name, archive_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(archive_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(archive_name))
+
+        if run('mkdir -p /data/web_static/releases/{}'.format
+                (archive_name)).failed is True:
+            return False
+        if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+           (file, archive_name)).failed is True:
+            return False
+        if run('rm /tmp/{}'.format(file)).failed is True:
+            return False
+        if run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format
+           (archive_name, archive_name)).failed is True:
+            return False
+        if run('rm -rf /data/web_static/releases/{}/web_static'
+           .format(archive_name)).failed is True:
+            return False
+        if run('rm -rf /data/web_static/current').failed is True:
+            return False
+        if run('ln -s /data/web_static/releases/{} /data/web_static/current'
+           .format(archive_name)).failed is True:
+            return False
         return(True)
     except Exception as e:
         return(False)
